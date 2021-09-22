f80554aa3dfbe1acf38a50af4c47f2169994966e
Safold <58793988+Safold@users.noreply.github.com>
2021-09-22 13:26:54 +0300

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,5 @@
 const output = document.querySelector('output')
+let old
 
 document.querySelectorAll('input').forEach(input => {
 	input.addEventListener('click', function(){
@@ -7,17 +8,25 @@ document.querySelectorAll('input').forEach(input => {
 })
 
 function calc(value) {
-	if(value === '='){
+	if(output.innerHTML == '' && !Number.isInteger(value)) {
+		alert('Error')
+		return
+	}
+	else if(String(output.innerHTML).slice(-1) == '*'|'/'|'+'|'-' && !Number.isInteger(value)){
+		alert('Error')
+		return
+	}
+	if(value == '='){
 		output.innerHTML = eval(output.innerHTML)
 	}
-	else if(value === '^2'){
+	else if(value == '^2' && output.innerHTML != ' '){
 		output.innerHTML = Math.pow(eval(output.innerHTML), 2)
 	}
 	else{
 		output.innerHTML += value
 	}
-}
 
+}
 function transform(string){
 	if(!Number.isInteger(parseInt(string))){
 		return(string)
